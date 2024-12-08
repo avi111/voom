0abc91941f694d614f2586829c103e3900f63cf7
Avi Levkovich <avi111@gmail.com>
2024-12-08 10:20:28 +0200

    continue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,9 +39,15 @@ const Home = () => {
         </div>
       ) : (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-          {articles.map((article) => (
-            <NewsCard key={article.url} article={article} />
-          ))}
+          {articles.length ? (
+            articles.map((article) => (
+              <NewsCard key={article.url} article={article} />
+            ))
+          ) : (
+            <p className='text-center text-gray-600 mt-8'>
+              No articles found for this search term.
+            </p>
+          )}
         </div>
       )}
     </>
